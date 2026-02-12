b62f1ad96c6bc84e23a6c52feac86f798dac1db9
Wisse Hes <wisse.hes@ah.nl>
2026-02-12 15:10:47 +0100

    Initial Commit

---
+++ METHANE/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  METHANE
+//
+//  Created by Wisse Hes on 12/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ METHANE/METHANEApp.swift
@@ -0,0 +1,17 @@
+//
+//  METHANEApp.swift
+//  METHANE
+//
+//  Created by Wisse Hes on 12/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct METHANEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
